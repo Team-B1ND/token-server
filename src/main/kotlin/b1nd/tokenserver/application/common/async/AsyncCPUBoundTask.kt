dc8e22c698c32cefb71ecfef50123c73f8749b7c
최수원 <suwon3274@gmail.com>
2024-06-24 18:08:44 +0900

    refactor: Schedulers.parallel() 사용

--- src/main/kotlin/b1nd/tokenserver/application/common/async/AsyncCPUBoundTask.kt
+++ src/main/kotlin/b1nd/tokenserver/application/common/async/AsyncCPUBoundTask.kt
@@ -1,6 +1,7 @@
 package b1nd.tokenserver.application.common.async
 
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import java.util.concurrent.Callable
 
 class AsyncCPUBoundTask {
@@ -9,6 +10,7 @@ class AsyncCPUBoundTask {
 
         fun <T> execute(supplier: Callable<T>): Mono<T> {
             return Mono.fromCallable(supplier)
+                .subscribeOn(Schedulers.parallel())
         }
 
     }
